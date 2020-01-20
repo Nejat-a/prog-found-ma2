1fc731e3ef454ed5d727bae53c84d390ff347d03
Nejat-a <55981343+Nejat-a@users.noreply.github.com>
2020-01-20 22:09:23 +0100

    added script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+// question 2
+console.log(petArray[1]);
+// question 3
+petArray.push("sheep");
+// question 4
+console.log(petArray.length);
+// question 5
+var catObject = {
+  name: "Truls",
+  colour: "brown",
+  age: 7
+};
+// question 6
+console.log(catObject.age);
+// question 7
+var catArray = [
+  {
+    name: "Truls",
+    colour: "brown",
+    age: 7
+  }
+];
+// question 8
+for (i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+// question 9
+function logToConsole(aniamals) {
+  console.log(aniamals);
+}
+logToConsole("dog");
+// question 10
+logToConsole("donkey")
+// question
+// question
